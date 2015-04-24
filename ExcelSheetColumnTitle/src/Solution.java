100acc63d74808717cd360f7bf517f2c817305b5
Chris Parkin <chris.d.parkin@gmail.com>
2015-04-24 14:59:06 -0400

    Excel Sheet Column Title (Z => 26, AA => 27...)

---
+++ ExcelSheetColumnTitle/src/Solution.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+	
+	public static String convertToTitle(int n) {
+		StringBuffer result = new StringBuffer();
+		while(n > 0){
+			if(n % 26 == 0){
+				result.append('Z');
+				n = n / 26 - 1;
+				continue;
+			}else{
+				result.append((char)('A' + n%26 - 1));
+			}
+			n = n / 26;
+		}
+		return result.reverse().toString();
+    }
+	
+	public static void main(String[] args){
+		for(int i = 1; i < 256; i++){
+			System.out.println(i + ": " + convertToTitle(i));
+		}
+	}
+}
